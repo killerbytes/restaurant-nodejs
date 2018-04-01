cdc4ed155c13c3c444486c6734c37cbf2a7763f9
Joel Carlos <joelcarlos02@gmail.com>
2018-04-01 23:36:22 +0800

    table module

--- controllers/tables.js
+++ controllers/tables.js
@@ -4,25 +4,60 @@ const Table = require('../models').table;
 
 module.exports = {
 
-  list(){
-    return new Promise((resolve, reject)=>{
+  list() {
+    return new Promise((resolve, reject) => {
       Table
         .findAll({
           include: [
           ]
         })
-        .then(res=> resolve(res))
-        .catch(error=>reject(error))
+        .then(res => resolve(res))
+        .catch(error => reject(error))
     })
   },
 
 
-  get(id){
-    return new Promise((resolve, reject)=>{
+  get(id) {
+    return new Promise((resolve, reject) => {
       Table.findById(id)
-        .then(res=> resolve(res))
-        .catch(error=> reject(error))
+        .then(res => resolve(res))
+        .catch(error => reject(error))
     })
   },
-  
+  create({ name, order }) {
+    return Table.create({
+      name,
+      order
+    })
+  },
+  update(id, data) {
+    return new Promise((resolve, reject) => {
+      Table
+        .findById(id)
+        .then(item => {
+          item.update(data)
+            .then(res => resolve(res))
+            .catch(error => reject(error))
+        })
+        .catch(error => reject(error))
+
+    })
+  },
+
+  delete(id) {
+    return new Promise((resolve, reject) => {
+
+      Table.findById(id)
+        .then(table => {
+          if (table) {
+            resolve(table.destroy())
+          } else {
+            reject(new Error('Table not found'))
+          }
+        })
+        .catch(err => reject(err))
+    })
+
+
+  }
 }
--- routes/api/tables.js
+++ routes/api/tables.js
@@ -11,12 +11,21 @@ router.get('/', function (req, res, next) {
   })
 });
 
+router.post('/', function (req, res, next) {
+  tablesController.create(req.body)
+    .then(table => {
+      res.status(201).send(table)
+    })
+    .catch(err => {
+      res.status(400).send(error.response(400, err.message))
+    })
+})
 router.get('/:id', function (req, res, next) {
   tablesController.get(req.params.id)
     .then(table => {
       if (table) {
 
-        res.send({ item: table })
+        res.send(table)
       } else {
         res.status(404).send(error.response(404, "Table not found"))
       }
@@ -24,6 +33,26 @@ router.get('/:id', function (req, res, next) {
 
 });
 
+
+router.patch('/:id', function (req, res, next) {
+  tablesController.update(req.params.id, req.body)
+    .then(table => {
+      res.status(202).send(table)
+    })
+    .catch(err => {
+      res.status(400).send(error.response(400, err.message))
+    })
+})
+
+router.delete('/:id', function (req, res, next) {
+  tablesController.delete(req.params.id)
+    .then(() => {
+      res.status(204).send()
+    })
+    .catch(err => {
+      res.status(404).send(error.response(404, err.message))
+    })
+});
 
 
 module.exports = router;
